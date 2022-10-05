2e63c290246519163adf4fc9d7dde7cd6491186e
blizzark <blizzark@ya.ru>
2022-10-05 22:53:47 +0300

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,234 @@
+﻿namespace Lab1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            this.label4 = new System.Windows.Forms.Label();
+            this.trackBar3 = new System.Windows.Forms.TrackBar();
+            this.label5 = new System.Windows.Forms.Label();
+            this.trackBar4 = new System.Windows.Forms.TrackBar();
+            this.label6 = new System.Windows.Forms.Label();
+            this.trackBar5 = new System.Windows.Forms.TrackBar();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar5)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(74, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(120, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Настройки";
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(30, 115);
+            this.trackBar1.Minimum = -10;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(203, 45);
+            this.trackBar1.TabIndex = 0;
+            this.trackBar1.Value = 1;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(27, 85);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(208, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Скорость вращения Cолнца: 1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(50, 165);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(165, 34);
+            this.label3.TabIndex = 3;
+            this.label3.Text = " Скорость вращения \r\nЗемли вокруг Солнца: 1\r\n";
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.Location = new System.Drawing.Point(30, 202);
+            this.trackBar2.Minimum = -10;
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Size = new System.Drawing.Size(203, 45);
+            this.trackBar2.TabIndex = 2;
+            this.trackBar2.Value = 1;
+            this.trackBar2.Scroll += new System.EventHandler(this.trackBar2_Scroll);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(50, 259);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(147, 34);
+            this.label4.TabIndex = 5;
+            this.label4.Text = " Скорость вращения \r\n Земли вокруг оси: 1";
+            // 
+            // trackBar3
+            // 
+            this.trackBar3.Location = new System.Drawing.Point(30, 296);
+            this.trackBar3.Maximum = 50;
+            this.trackBar3.Minimum = -50;
+            this.trackBar3.Name = "trackBar3";
+            this.trackBar3.Size = new System.Drawing.Size(203, 45);
+            this.trackBar3.TabIndex = 4;
+            this.trackBar3.Value = 1;
+            this.trackBar3.Scroll += new System.EventHandler(this.trackBar3_Scroll);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(50, 352);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(155, 34);
+            this.label5.TabIndex = 7;
+            this.label5.Text = " Скорость вращения \r\n Луны вокруг Земли: 1";
+            // 
+            // trackBar4
+            // 
+            this.trackBar4.Location = new System.Drawing.Point(30, 389);
+            this.trackBar4.Maximum = 50;
+            this.trackBar4.Minimum = -50;
+            this.trackBar4.Name = "trackBar4";
+            this.trackBar4.Size = new System.Drawing.Size(203, 45);
+            this.trackBar4.TabIndex = 6;
+            this.trackBar4.Value = 1;
+            this.trackBar4.Scroll += new System.EventHandler(this.trackBar4_Scroll);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(50, 443);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(147, 34);
+            this.label6.TabIndex = 9;
+            this.label6.Text = " Скорость вращения \r\n Луны вокруг оси: 1\r\n";
+            // 
+            // trackBar5
+            // 
+            this.trackBar5.Location = new System.Drawing.Point(30, 480);
+            this.trackBar5.Maximum = 50;
+            this.trackBar5.Minimum = -50;
+            this.trackBar5.Name = "trackBar5";
+            this.trackBar5.Size = new System.Drawing.Size(203, 45);
+            this.trackBar5.TabIndex = 8;
+            this.trackBar5.Value = 1;
+            this.trackBar5.Scroll += new System.EventHandler(this.trackBar5_Scroll);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(41, 553);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(174, 47);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Разработчики";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.trackBar5);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.trackBar4);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.trackBar3);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.trackBar2);
+            this.groupBox1.Controls.Add(this.trackBar1);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(716, 28);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(256, 629);
+            this.groupBox1.TabIndex = 11;
+            this.groupBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(984, 680);
+            this.Controls.Add(this.groupBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Солнечная система";
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar5)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TrackBar trackBar2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TrackBar trackBar3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TrackBar trackBar4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TrackBar trackBar5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Lab1
+{
+    public partial class Form1 : Form
+    {
+        private Image Sol;
+        private Image Earth;
+        private Image Moon;
+        float IntervalR = 0; 
+        float IntervalEar = 0; 
+        float IntervalRotateEar = 0;
+        float IntervalRotateMoon = 0;
+        float IntervalMoonOs = 0; 
+
+        public Form1()
+        {
+            InitializeComponent();
+            DoubleBuffered = true;
+            Earth = Image.FromFile("earth.png");
+            Moon = Image.FromFile("moon.png");
+            Sol = Image.FromFile("sol.png");
+            ///////////
+            var t = new Timer();// таймер вращения солнца
+            t.Interval = 1; 
+            t.Enabled = true;
+            t.Tick += (s, o) => { IntervalR += trackBar1.Value; Invalidate(); };
+            /////////
+            var ear = new Timer();//таймер вращения земли вокруг солнца
+            ear.Interval = 1;
+            ear.Enabled = true;
+            ear.Tick += (s, o) => { IntervalEar += (float)trackBar2.Value/100; Invalidate(); };
+            //////////
+            var RotateEar = new Timer();// таймер вращения Земли вокруг своей оси
+            RotateEar.Interval = 1;
+            RotateEar.Enabled = true;
+            RotateEar.Tick += (s, o) => { IntervalRotateEar += trackBar3.Value; Invalidate(); };
+            /////////s
+            var RotateMoon = new Timer();// таймер вращения Луны вокруг Земли
+            RotateMoon.Interval = 1;
+            RotateMoon.Enabled = true;
+            RotateMoon.Tick += (s, o) => { IntervalRotateMoon += (float)trackBar4.Value/100; Invalidate(); };
+            /////////
+            var MoonOs = new Timer();// таймер вращения Луны вокруг своей оси
+            MoonOs.Interval = 1;
+            MoonOs.Enabled = true;
+            MoonOs.Tick += (s, o) => { IntervalMoonOs += trackBar5.Value; Invalidate(); };
+        }
+
+        protected override void OnPaint(PaintEventArgs e) // Рисовалка
+        {
+            e.Graphics.Transform = new System.Drawing.Drawing2D.Matrix(1, 0, 0, 1, 380, 340);
+            // Солнце
+                var s = e.Graphics;
+                s.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+                s.RotateTransform(IntervalR);
+                s.DrawImage(Sol, -Sol.Width / 2, -Sol.Height / 2);
+            
+            // Земля
+            e.Graphics.Transform = new System.Drawing.Drawing2D.Matrix(1, 0, 0, 1, 380, 340);
+                double x2 = 320;
+                double y2 = 320;
+                double[,] SolMatrix = raschotEar(IntervalEar, x2, y2);
+                var ear = e.Graphics;
+                ear.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+                ear.TranslateTransform((float)SolMatrix[0, 0] / 2, (float)SolMatrix[1, 0] / 2);
+                ear.RotateTransform(IntervalRotateEar);
+                ear.DrawImage(Earth, -Earth.Width / 2, -Earth.Height / 2);
+             //
+            {   // Луна
+                e.Graphics.Transform = new System.Drawing.Drawing2D.Matrix(1, 0, 0, 1, (float)SolMatrix[0, 0] / 2 + 380, (float)SolMatrix[1, 0] / 2 + 340);
+                var moon = e.Graphics;
+                moon.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+                double[,] MoonMatrix = raschotEar(IntervalRotateMoon, 130, 130);
+                moon.TranslateTransform((float)MoonMatrix[0, 0] / 2, (float)MoonMatrix[1, 0] / 2);
+                moon.RotateTransform(IntervalMoonOs);
+                moon.DrawImage(Moon, -Moon.Width / 2, -Moon.Height / 2);
+            }
+        }
+
+        public static double[,] raschotEar(double fi, double x1, double y1) // Рассчёт координат
+        {
+            double[,] aMatrix = new double[2, 2];
+            double[,] bMatrix = new double[2, 1];
+            aMatrix[0, 0] = Math.Cos(fi);
+            aMatrix[0, 1] = -Math.Sin(fi);
+            aMatrix[1, 0] = Math.Sin(fi);
+            aMatrix[1, 1] = Math.Cos(fi);
+            bMatrix[0, 0] = x1;
+            bMatrix[1, 0] = y1;
+            double[,] cMatrix = new double[aMatrix.GetLength(0), bMatrix.GetLength(1)];
+            for (int i = 0; i < aMatrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < bMatrix.GetLength(1); j++)
+                {
+                    for (int k = 0; k < bMatrix.GetLength(0); k++)
+                    {
+                        cMatrix[i, j] += aMatrix[i, k] * bMatrix[k, j];
+                    }
+                }
+            }
+            return cMatrix;
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            label2.Text = String.Format("Скорость вращения Cолнца: {0}", trackBar1.Value);
+        }
+        private void trackBar2_Scroll(object sender, EventArgs e)
+        {
+            label3.Text = String.Format(" Скорость вращения\nЗемли вокруг Солнца: {0}", trackBar2.Value);
+        }
+        private void trackBar3_Scroll(object sender, EventArgs e)
+        {
+            label4.Text = String.Format(" Скорость вращения\n Земли вокруг оси: {0}", trackBar3.Value);
+            
+        }
+        private void trackBar4_Scroll(object sender, EventArgs e)
+        {
+            label5.Text = String.Format(" Скорость вращения\n Луны вокруг Земли: {0}", trackBar4.Value);
+        }
+        private void trackBar5_Scroll(object sender, EventArgs e)
+        {
+            label6.Text = String.Format(" Скорость вращения\n Луны вокруг оси: {0}", trackBar5.Value);
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Данную программу разработали студенты\nСПбГТИ(ТУ) факультета ИТиУ 475группы:\nОвчинников Роман Сергеевич\nАндрианова Карина Ивановна\nПекер Валерия Александровна", "Информация о разработчике");
+        }
+    }
+}
